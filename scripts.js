d2b787817ac0531be2e826a1750846d360eab988
Yaa Otchere <yaa@yaaotchere.ca>
2020-04-26 09:40:39 -0400

    initial commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+var ipsum = [
+    `The ten-dollar Founding Father without a father`,
+    `My name is Alexander Hamilton and there's a million things I haven't done`,
+    `In New York you can be a new man`,
+    `Boy you got me helpless`,
+    `I have the honour to be your obedient servant`,
+    `Dear Theodosia what to say to you`,
+    `Alexander Hamilton had a torrid affair and he wrote down right here`,
+    `The charge against me is one for improper speculation`,
+    `My real crime is an amorous connection with his wife - for a considerable time with his knowing consent`,
+    `Just you wait`,
+];
+
+console.log(ipsum);
+
