f2582bf0a88b9f68370a6bd9272447904d5d6267
Yaa Otchere <yaa@yaaotchere.ca>
2020-04-26 11:29:55 -0400

    make a readme file

--- scripts.js
+++ scripts.js
@@ -1,18 +1,25 @@
+var myShotPrefix = `I am not giving up my shot! `;
+
+var swearing = [
+    `Bastard.`,
+    `Son of a whore.`,
+    `Sit down John you fat mother fucker.`,
+    `Orphan.`
+]
+
 var ipsum = [
-    `The ten-dollar Founding Father without a father`,
-    `My name is Alexander Hamilton and there's a million things I haven't done`,
-    `In New York you can be a new man`,
-    `Boy you got me helpless`,
-    `I have the honour to be your obedient servant`,
-    `Dear Theodosia what to say to you`,
-    `Alexander Hamilton had a torrid affair and he wrote down right here`,
-    `The charge against me is one for improper speculation`,
-    `My real crime is an amorous connection with his wife - for a considerable time with his knowing consent`,
-    `Just you wait`,
+    `The ten-dollar Founding Father without a father.`,
+    `My name is Alexander Hamilton and there's a million things I haven't done.`,
+    `In New York you can be a new man.`,
+    `Boy you got me helpless.`,
+    `I have the honour to be your obedient servant.`,
+    `Dear Theodosia what to say to you?`,
+    `Alexander Hamilton had a torrid affair and he wrote down right here.`,
+    `The charge against me is one for improper speculation.`,
+    `My real crime is an amorous connection with his wife - for a considerable time with his knowing consent.`,
+    `Just you wait!`,
 ];
 
 function scramble() {
-    console.log(ipsum[3]);
+    console.log(`${ipsum[3]}`);
 }
-
-scramble();
