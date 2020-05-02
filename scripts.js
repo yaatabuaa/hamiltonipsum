24d844cc745390c34e0b0d28039b988057f905d5
Yaa Otchere <yaa@yaaotchere.ca>
2020-05-02 12:49:12 -0400

    working on arrays

--- scripts.js
+++ scripts.js
@@ -8,30 +8,72 @@ var swearing = [
 ]
 
 var ipsum = [
-    `The ten-dollar Founding Father without a father.`,
-    `My name is Alexander Hamilton and there's a million things I haven't done.`,
-    `In New York you can be a new man.`,
-    `Boy you got me helpless.`,
-    `I have the honour to be your obedient servant.`,
-    `Dear Theodosia what to say to you?`,
-    `Alexander Hamilton had a torrid affair and he wrote down right here.`,
-    `The charge against me is one for improper speculation.`,
-    `My real crime is an amorous connection with his wife - for a considerable time with his knowing consent.`,
-    `Just you wait!`,
+    //My Shot
+    `Hey yo, I'm just like my country`,
+    `I'm young, scrappy, and hungry`,
+    `And I'm not throwing away my shot`,
+    `I'mma get scholarship to King's College`,
+    `I probably shouldn't brag, but dag, I amaze and astonish`,
+    `The problem is I got a lot of brains, but no polish`,
+    `I gotta holler just to be heard`,
+    `With every word I drop knowledge`,
+    `I'm a diamond in the rough, a shiny piece of coal`,
+    `Tryin' to reach my goal, my power of speech: unimpeachable`,
+    `Only nineteen, but my mind is older`,
+    `These New York City streets getting colder, I shoulder`,
+    `Ev'ry burden, ev'ry disadvantage`,
+    `I have learned to manage. I don't have a gun to brandish`,
+    `I walk these streets famished`,
+    `The plan is to fan this spark into a flame`,
+    `But damn, it's getting dark, so let me spell out my name`,
+    `I am the - A-L, E-X, A-N, D E-R—we are—meant to be`,
+    `A colony that runs independently`,
+    `Meanwhile, Britain keeps shitting on us endlessly`,
+    `Essentially, they tax us relentlessly`,
+    `Then King George turns around, runs a spending spree`,
+    `He ain't never gonna set his descendants free`,
+    `So there will be a revolution in this century`,
+    `ENTER ME!`,
+    `(He says in parentheses)`,
+    `Don't be shocked when your history book mentions me`,
+    `I will lay down my life if it sets us free`,
+    `Eventually you'll see my ascendancy`,
+    `And I am not throwing away my shot (my shot)`,
+    `I am not throwing away my shot (my shot)`,
+    `Hey yo, I'm just like my country`,
+    `I'm young, scrappy, and hungry`,
+    `I am not throwing away my shot`,
+    `I am not throwing away my shot`,
+    `Hey yo, I'm just like my country`,
+    `I'm young, scrappy, and hungry`,
+    `And I'm not throwing away my shot`,
+    `It's time to take a shot!`,
 ];
 
+//set the default value on #paragraphs to 3
 document.getElementById('paragraphs').defaultValue = 3;
 
 var paragraphs = document.getElementById('paragraphs');
 
-paragraphs.addEventListener('change', updateParagraphs);
+paragraphs.addEventListener('change', updateParagraphNumber);
 
-function updateParagraphs() {
-    //if they enter 0, change it to 1
-    console.log(paragraphs.value);
+function updateParagraphNumber() {
+    // will need a for loop i think, for number of paragraphs generate p tags
+    console.log(`${paragraphs.value}`);
 }
+
+//appending the array to I am not throwing away my shot
+// document.getElementById("my-shot").defaultValue = false;
+
+// function generateMyShotPrefix() {
+//     var myShotCheckbox = document.getElementById("my-shot");
+//     if (myShotCheckbox.checked == true) {
+//         ipsum.unshift(`${myShotPrefix}`);
+//     }
+// }
 
 function loremIpsum() {
-    document.getElementById('generated-text').innerHTML = `${ipsum}`;
+    document.getElementById('generated-text').innerHTML = `<h1>Hamilton the Musical Words here baby!</h1>`;
 }
-// numberOfParagraphs();
+
+// generateMyShotPrefix();
